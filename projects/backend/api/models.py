e76cf4c74778a7745a3ac92ca8953ae97b880960
Veljko <wex@tilda.center>
2017-08-11 12:31:36 +0200

    Fix backend

--- projects/backend/api/models.py
+++ projects/backend/api/models.py
@@ -4,13 +4,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(unique=True, max_length=20)
-    owner = models.ForeignKey('User')
-    
+    owner = models.ForeignKey(User)
+
 class Ticket(models.Model):
     name = models.CharField(max_length=20)
     description = models.TextField()
     date = models.DateField()
     duration = models.IntegerField()
-    project = models.ForeignKey('Project')
-    owner = models.ForeignKey('User')
-    
+    project = models.ForeignKey(Project)
+    owner = models.ForeignKey(User)
